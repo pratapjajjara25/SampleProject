faeb964d9bef47700200e56b6f114cd6146fa271
Pratapjajjara25 <pratapk31@gmail.com>
2023-08-14 20:25:14 +0530

    first commit

---
+++ src/test/java/kalamsir/SampleProgram.java
@@ -0,0 +1,13 @@
+package kalamsir;
+
+import org.testng.annotations.Test;
+
+public class SampleProgram
+{
+
+	@Test
+	public void method()
+	{
+		System.out.println("hi welcome to new project");
+	}
+}

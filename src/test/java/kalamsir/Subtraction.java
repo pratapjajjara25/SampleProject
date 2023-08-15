d909a8a47cdaafd8dad8747074482e7b87110625
Pratapjajjara25 <pratapk31@gmail.com>
2023-08-15 09:28:44 +0530

    my third commit

---
+++ src/test/java/kalamsir/Subtraction.java
@@ -0,0 +1,14 @@
+package kalamsir;
+
+import org.testng.annotations.Test;
+
+public class Subtraction 
+{
+	@Test
+	public void sub()
+	{
+		int x= 10,y=20;
+		int z=x-y;
+		System.out.println("subtraction is " +z);
+	}
+}

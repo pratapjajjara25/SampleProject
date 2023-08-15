efb083d854fcb6d2ae3d6ac3c8a40dc14f459cab
Pratapjajjara25 <pratapk31@gmail.com>
2023-08-15 09:08:04 +0530

    my second commit

---
+++ src/test/java/kalamsir/Addition.java
@@ -0,0 +1,12 @@
+package kalamsir;
+
+public class Addition 
+{
+	public static void main(String[] args) 
+	{
+		int x= 10;
+		int y =20;
+		int z =x+y;
+		System.out.println("addition is " +z);
+	}
+}
